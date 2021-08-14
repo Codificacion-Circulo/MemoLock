9e0c7ddee36dcf8dc4e2138662a7fbdcd81c4d10
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-08-14 13:11:38 +0530

    fixed minor issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import Tilt from 'react-parallax-tilt';
 import ethereum from '../images/ethereum1.png'
 import ipfs from '../images/ipfs.png';
 import Faqs from '../components/Faqs'
-
 import './Home.css'
 
 
@@ -15,24 +14,24 @@ const Home = () => {
 
     return (
         <Fragment>
-        <section id="home" class="home">
-        <div class="home-con">
+        <section id="home" className="home">
+        <div className="home-con">
        
             <h1>Create a blog and share your voice in minutes.</h1>
             <h2>Extracting your center of creativity and preserving it forever on the blockchain.</h2>
         </div>
     </section>
 
-    <section id="features" class="features">
-        <div class="heading">
+    <section id="features" className="features">
+        <div className="heading">
             <h1>FEATURES</h1>
         </div>
-        <div class="container">
+        <div className="container">
         <Tilt className="parallax-effect" perspective={1000}>
         
-            <div class="card" >
+            <div className="card" >
             <div className="inner-element">
-                <div class="card_img"><i class="fab fa-ethereum"></i></div>
+                <div className="card_img"><i className="fab fa-ethereum"></i></div>
                 <h1>NFT</h1>
                 <p>
                     Convert blogs to nfts.
@@ -41,9 +40,9 @@ const Home = () => {
             </div>
         </Tilt>
         <Tilt className="parallax-effect" perspective={1000}>
-            <div class="card">
+            <div className="card">
              <div className="inner-element">
-                <div class="card_img"><i class="fas fa-file-archive"></i></div>
+                <div className="card_img"><i className="fas fa-file-archive"></i></div>
                 <h1>Immutable</h1>
                 <p>
                     Remains unchanged.
@@ -52,9 +51,9 @@ const Home = () => {
             </div>
             </Tilt>
             <Tilt className="parallax-effect" perspective={1000}>
-            <div class="card">
+            <div className="card">
              <div className="inner-element">
-                <div class="card_img"><i class="fas fa-crown"></i></div>
+                <div className="card_img"><i className="fas fa-crown"></i></div>
                 <h1>Sole Proprietary </h1>
                 <p>
                     Exclusive owner of blog.
@@ -63,9 +62,9 @@ const Home = () => {
             </div>
             </Tilt>
             <Tilt className="parallax-effect" perspective={1000}>
-            <div class="card">
+            <div className="card">
              <div className="inner-element">
-                <div class="card_img"><i class="fas fa-shield-alt"></i></div>
+                <div className="card_img"><i className="fas fa-shield-alt"></i></div>
                 <h1>Enduring</h1>
                 <p>
                     Secure on blockchain forever.
@@ -74,9 +73,9 @@ const Home = () => {
             </div>
             </Tilt>
             <Tilt className="parallax-effect" perspective={1000}>
-            <div class="card">
+            <div className="card">
              <div className="inner-element">
-                <div class="card_img"><i class="fas fa-user-secret"></i></div>
+                <div className="card_img"><i className="fas fa-user-secret"></i></div>
                 <h1>Anonymous</h1>
                 <p>
                     Keep your identity private.
@@ -88,26 +87,26 @@ const Home = () => {
         
     </section>
 
-    <section id="tech" class="tech">
-        <div class="tech-con">
-            <div class="tech-heading">
+    <section id="tech" className="tech">
+        <div className="tech-con">
+            <div className="tech-heading">
                 <h1>TRUE OWNERSHIP</h1>
             </div>
-            <div class="tech-para">
+            <div className="tech-para">
                 <p>Stored on IPFS and Secured by Ethereum Blockchain</p>
             </div>
-            <div class="tech-icons">
+            <div className="tech-icons">
             <img src={ethereum} alt="etherium"/>
                 <img src={ipfs} alt="ipfs" style={{width:'125px', height:'125px'}}/>
             </div>
-            <div class="tech-btn">
-                <button><a href="ipfs.com">Know More</a><i class="fas fa-arrow-right"></i></button>
+            <div className="tech-btn">
+                <button><a href="ipfs.com">Know More</a><i className="fas fa-arrow-right"></i></button>
             </div>
         </div>
     </section>
 
-    <section id="faq" class="faq">
-    <div class="faq-heading">
+    <section id="faq" className="faq">
+    <div className="faq-heading">
                 <h1>FAQs</h1>
             </div>
        <Faqs/>
