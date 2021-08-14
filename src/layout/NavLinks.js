5ee9176f59c5774ec3cd25ed36d3427c8ac5aadd
Sreyom31 <sreyom31@gmail.com>
2021-08-15 01:27:40 +0530

    add hamburger menu in the navbar and made it responsive

---
+++ src/layout/NavLinks.js
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import classes from './Header.module.css'
+function NavLinks(props) {
+    return (
+        <ul>
+        <NavLink to='/home' activeClassName={classes.active} style={{ textDecoration: 'none'}} onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <li>HOME</li>
+        </NavLink>
+
+        <NavLink to='/create' activeClassName={classes.active} style={{ textDecoration: 'none' }} onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <li>WRITE</li>
+        </NavLink>
+
+        <NavLink to='/view' activeClassName={classes.active} style={{ textDecoration: 'none' }} onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <li>VIEW</li>
+        </NavLink>
+
+        <NavLink to='/*' activeClassName={classes.active} style={{ textDecoration: 'none' }} onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <li>ABOUT</li>
+        </NavLink>
+        </ul>
+    );
+}
+
+export default NavLinks;
