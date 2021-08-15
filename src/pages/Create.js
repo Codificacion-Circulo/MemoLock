88865c9184dbc7bf4b8f064476b6643c972ac6bf
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-08-15 19:55:38 +0530

    use effect from create and navlink from header removed

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React from 'react';
 // import { useWeb3React } from "@web3-react/core"
 // import { injected } from "../wallet/connectors"
 import './Create.css'
