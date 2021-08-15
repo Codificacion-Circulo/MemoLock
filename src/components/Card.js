5cd1e7140df5ab2daeb060d744f9c92351303aa2
Sreyom31 <sreyom31@gmail.com>
2021-08-15 21:45:40 +0530

    edit the modals design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import Modal from './Modal';
 import classes from './Card.module.css';
+import {Link} from 'react-router-dom'
 
 const Card = (props) => {
   return (
     <Modal onClose={props.onClose}>
-       <h4 className={classes.Head}>WARNING!<span className={classes.Headspan}>-This is a Secure Link Password Protected</span> </h4>
+       <h4 className={classes.Head}>NOTICE!<span className={classes.Headspan}> &nbsp;-&nbsp;Save this link for future use.</span> </h4>
       <div className={classes.total}>
-        <span>{`http://localhost:3000/view/${props.link}`}</span>
+        <Link to={`http://localhost:3000/view/${props.link}`}>{`http://localhost:3000/view/${props.link}`}</Link>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
--- src/pages/View.js
+++ src/pages/View.js
@@ -1,57 +1,18 @@
-import {Fragment,useState,useCallback, useEffect} from 'react';
-import {Link, useParams} from 'react-router-dom'
-import LoadingSpinner from '../components/LoadingSpinner'
+import {Fragment} from 'react';
+import {useParams} from 'react-router-dom'
+// import LoadingSpinner from '../components/LoadingSpinner'
 import classes from './View.module.css'
 // import { useWeb3React } from "@web3-react/core"
 // import { injected } from "../wallet/connectors"
-const Cryptr = require('cryptr');
-
-
-
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
 
 const View =props => {
 
   // const { active, account, library, connector, activate, deactivate } = useWeb3React()
-  const [blog,setBlogs]=useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const params = useParams();
-
-  
-      const fetchBlogs=useCallback(async()=>{
-        setIsLoading(true);
-        setError(null);
-        // await activate(injected)
-        if(true){
-          try{
-            const cryptr = new Cryptr('123');
-            const decryptedString = cryptr.decrypt(params.id);
-            console.log(decryptedString)
-            const response = await fetch(`${decryptedString}`,requestOptions);
-            if (!response.ok) {
-              throw new Error('Something went wrong!');
-            }
-            const data = await response.url;
-            console.log(data)
-            setBlogs(data)
-          }catch(error){
-            setError(error.message);
-          }
-          setIsLoading(false)
-        }
-        
-      },[]);
-    useEffect(() => {
-        fetchBlogs();
-    }, [fetchBlogs])
 
     return (
       <Fragment>
-       {isLoading&&<LoadingSpinner/>}
+       {/* {isLoading&&<LoadingSpinner/>} */}
         <section className={classes.info}>
         <div className={classes.heading}>
             <h1>{params.id}</h1>
